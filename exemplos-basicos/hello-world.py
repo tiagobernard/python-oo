f07da9df51df5a8e2c5262833436e320b8438ce6
tiagobernard <tiago@tiagobernardes.com.br>
2025-01-09 13:40:52 -0300

    Subindo o Hello Horld

---
+++ exemplos-basicos/hello-world.py
@@ -0,0 +1,10 @@
+class HelloWorld:
+    def __init__(self, message):
+        self.message = message
+
+    def display_message(self):
+        print(self.message)
+
+#Criando uma instância da classe HelloWorld
+hello = HelloWorld("Hello, World!")
+hello.display_message()
