286008e6f0feb1fd7b26617825cc0279677e200b
tiagobernard <tiago@tiagobernardes.com.br>
2025-01-09 20:56:14 -0300

    Subindos arquivos estruturados

---
+++ exemplos-basicos/estrutura.py
@@ -0,0 +1,10 @@
+
+from estrutura_classes import Vendedor
+
+vendedor1 = Vendedor("Tiago")
+vendedor1.vendeu(1000)
+vendedor1.bateu_meta(600)
+
+vendedor2 = Vendedor("Aristides")
+vendedor2.vendeu(300)
+vendedor2.bateu_meta(600)
