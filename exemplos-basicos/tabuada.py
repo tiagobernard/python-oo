0df453a60e1b32189f7a64c09a6bb4f3b99da9ac
tiagobernard <tiago@tiagobernardes.com.br>
2025-01-10 12:15:01 -0300

    Comnetando o código

--- exemplos-basicos/tabuada.py
+++ exemplos-basicos/tabuada.py
@@ -1,13 +1,13 @@
-class Tabuada:
-    def __init__(self, numTab):
-        self.contador = 1
+class Tabuada: #Classe tabuada
+    def __init__(self, numTab): #método construtor
+        self.contador = 1 #parâmetros
         self.numTab = numTab
 
-    def calculaTab(self):
+    def calculaTab(self): #método
         while(self.contador <= 10):
             self.resultado = self.numTab * self.contador
-            print(f"{self.contador} x {self.numTab} = {self.resultado}")
             self.contador += 1
+            print(f"{self.contador} x {self.numTab} = {self.resultado}")
 
-tabuadaCalc = Tabuada(7)
-tabuadaCalc.calculaTab()
+tabuadaCalc = Tabuada(7) # Instância
+tabuadaCalc.calculaTab() # chama o método calculaTab
