59afc04ca737f759e5bdc92763b4e6e0d5e5f046
tiagobernard <tiago@tiagobernardes.com.br>
2025-01-17 16:47:21 -0300

    Inserindo o significado de SOLID

--- programador-lhama-remake/aula19_SOLID_D_inversao_da_dependencia/run.py
+++ programador-lhama-remake/aula19_SOLID_D_inversao_da_dependencia/run.py
@@ -1,3 +1,10 @@
+#Princípio da Responsabilidade Única (Single Responsibility Principle)
+#Princípio Aberto Fechado (Open-Closed Principle)
+#Princípio da Substituição de Liskov (Liskov Substitution Principle)
+#Princípio da Segregação de Interfaces (Interface Segregation Principle)
+#Princípio da Inversão de Dependência (Dependency Inversion Principle)
+
+
 from elementos.interfaces.elemento_interface import ElementoInterface
 from elementos.elemento import Elemento
 from elementos.elemento2 import Elemento2
